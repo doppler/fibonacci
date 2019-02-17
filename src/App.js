fb4cf044d13f306955735a932b42142b6a97e3a6
David Rose <doppler@gmail.com>
2019-02-17 09:07:14 -0500

    pass fibValue from App to Spokes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import fib from "./fib";
 import "./App.scss";
 
 const App = () => {
-  const numSpokes = 15;
+  const numSpokes = 10;
+  const fibValue = 12;
   return (
     <div id="App">
       <svg width={"100vmin"} height={"100vmin"}>
         {Array.from({ length: numSpokes }).map((_, i) => (
           <Spoke
+            fibValue={fibValue}
             style={{
               transform: `translate(50vmin, 50vmin) rotate(${(360 / numSpokes) *
                 (i + 1)}deg)`
@@ -20,8 +22,8 @@ const App = () => {
   );
 };
 
-const Spoke = ({ style }) => {
-  const sequence = fib(12);
+const Spoke = ({ style, fibValue }) => {
+  const sequence = fib(fibValue);
   console.log({ sequence });
   return sequence.slice(0, sequence.length - 2).map((n, i) => (
     <g style={style}>
